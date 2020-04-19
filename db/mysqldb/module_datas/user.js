0af4a6bc30b4b361665cffe01bdb740dcd311108
qiqf <723469354@qq.com>
2020-04-19 12:34:24 +0800

    chore(sync): 移除User模型的初始数据, 去除 eslint配置中的  "no-await-in-loop": "off" 项

--- db/mysqldb/module_datas/user.js
+++ db/mysqldb/module_datas/user.js
@@ -9,22 +9,5 @@
 
 module.exports = {
   NAME: "user",
-  DATA: [
-    {
-      uid: 10000,
-      uuid: "1234567",
-      email: "9116895@qq.com",
-      username: "root",
-      nickname: "官方",
-      password: "red_book_admin",
-    },
-    {
-      uid: 10001,
-      uuid: "7654321",
-      email: "12345@qq.com",
-      username: "root1",
-      nickname: "官方",
-      password: "red_book_admin",
-    },
-  ],
+  DATA: [],
 };
--- db/mysqldb/sync.js
+++ db/mysqldb/sync.js
@@ -12,7 +12,7 @@ const sequelize = require("./init");
 
 // PATCH  中必须是对应模型的名字
 // 导出的模型名字
-const PATCH = ["Tag"];
+const PATCH = ["User"];
 const { seed } = require("./define")(sequelize, PATCH);
 
 sequelize
