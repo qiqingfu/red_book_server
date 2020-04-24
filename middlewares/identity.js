234f65a2fd6ec68a62fe948bd27d6ce458682476
qiqf <723469354@qq.com>
2020-04-24 21:01:39 +0800

    style(identity): 修复 eslint 报错问题

--- middlewares/identity.js
+++ middlewares/identity.js
@@ -1,6 +1,7 @@
 /**
  * 身份鉴权路由中间件
  */
+/* eslint-disable import/no-unresolved */
 const config = require("@/config");
 const safety = require("@/util/safety");
 const createError = require("http-errors");
@@ -12,29 +13,6 @@ const STATUS = {
   normal: 0, // 正常访问接口
   expired: 1, // 登录已过期
   noPermission: 2, // 无权限访问
-};
-
-module.exports = function (ctx, next) {
-  const userToken = ctx.header[config.CLIENT.XSRF_HEADER_NAME] || "";
-  const userCookie = ctx.cookies.get(config.CLIENT.SESSION_KEY) || "";
-  debug(userToken);
-  debug(userCookie);
-
-  if (!userToken || !userCookie) {
-    ctx.throw(createError(401, SEND_CLIENT_401_STATUS_MESSAGE));
-  }
-
-  verifyUser(userToken, ctx.session)
-    .then((verifyResult) => {
-      next();
-    })
-    .catch((code) => {
-      if (code === STATUS.expired) {
-        ctx.body = { errno: LOGINEXPIRED, message: "身份已过期, 请重新登录" };
-      } else {
-        ctx.throw(createError(401, SEND_CLIENT_401_STATUS_MESSAGE));
-      }
-    });
 };
 
 function verifyUser(t, session) {
@@ -52,6 +30,31 @@ function verifyUser(t, session) {
       }
 
       reject(STATUS.noPermission);
+
+      return true;
     });
   });
 }
+
+module.exports = function identity(ctx, next) {
+  const userToken = ctx.header[config.CLIENT.XSRF_HEADER_NAME] || "";
+  const userCookie = ctx.cookies.get(config.CLIENT.SESSION_KEY) || "";
+  debug(userToken);
+  debug(userCookie);
+
+  if (!userToken || !userCookie) {
+    ctx.throw(createError(401, SEND_CLIENT_401_STATUS_MESSAGE));
+  }
+
+  verifyUser(userToken, ctx.session)
+    .then(() => {
+      next();
+    })
+    .catch((code) => {
+      if (code === STATUS.expired) {
+        ctx.body = { errno: LOGINEXPIRED, message: "身份已过期, 请重新登录" };
+      } else {
+        ctx.throw(createError(401, SEND_CLIENT_401_STATUS_MESSAGE));
+      }
+    });
+};
